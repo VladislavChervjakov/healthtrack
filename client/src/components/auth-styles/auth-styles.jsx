8267c40af71c75edb8b6374daadfebeb23bdfcbc
Vlad9999 <vladchervjakov@gmail.com>
2023-07-17 19:59:45 +0300

    Added responsivnes for auth pages

--- client/src/components/auth-styles/auth-styles.jsx
+++ client/src/components/auth-styles/auth-styles.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { devices } from "../../stylesConfig";
 
 export const AuthPageContainer = styled.div`
   display: flex;
@@ -16,12 +17,30 @@ export const FormContainer = styled.div`
   form {
     width: 60%;
   }
+
+  @media screen and (max-width: ${devices.tablet}) {
+    form {
+      width: 70%;
+    }
+  }
+
+  @media screen and (max-width: ${devices.mobileL}) {
+    form {
+      width: 90%;
+    }
+  }
 `;
 
 export const AuthHeading = styled.div`
   h1 {
     font-size: 32px;
     line-height: 32px;
+  }
+
+  @media screen and (max-width: ${devices.tablet}) {
+    h1 {
+      text-align: center;
+    }
   }
 `;
 
@@ -35,4 +54,9 @@ export const AuthBanner = styled.div`
   background-image: ${({ backgroundImage }) => `url(${backgroundImage});`};
   background-size: cover;
   background-repeat: no-repeat;
+  background-position: center;
+
+  @media screen and (max-width: ${devices.tablet}) {
+    display: none;
+  }
 `;
---
+++ client/src/stylesConfig.js
@@ -0,0 +1,20 @@
+export const devices = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+export const colors = {
+  textColor: "#28282B",
+};
+
+export const fontSizes = {
+  heading: "42px",
+  medium: "18px",
+  small: "14px",
+  extraSmall: "12px",
+};
